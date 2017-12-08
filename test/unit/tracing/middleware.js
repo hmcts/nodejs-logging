c6f9e613505656a60afc7a9c0596169a5e7bd599
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-08 12:54:46 +0100

    Extract constant

--- test/unit/tracing/middleware.js
+++ test/unit/tracing/middleware.js
@@ -6,6 +6,8 @@ const { expect, sinon } = require('../../chai-sinon')
 const { requestTracingMiddleware } = require('../../../log/tracing/middleware')
 const { REQUEST_ID_HEADER, ROOT_REQUEST_ID_HEADER, ORIGIN_REQUEST_ID_HEADER } = require('../../../log/tracing/headers')
 
+const UUID_REGEX = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+
 describe('requestTracingMiddleware', () => {
   let req, res, next
 
@@ -24,12 +26,12 @@ describe('requestTracingMiddleware', () => {
 
   it('should set Request-Id header with UUID', () => {
     requestTracingMiddleware(req, req, next)
-    expect(req.headers[REQUEST_ID_HEADER]).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
+    expect(req.headers[REQUEST_ID_HEADER]).to.match(UUID_REGEX)
   })
 
   it('should set Root-Request-Id header with UUID', () => {
     requestTracingMiddleware(req, req, next)
-    expect(req.headers[ROOT_REQUEST_ID_HEADER]).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
+    expect(req.headers[ROOT_REQUEST_ID_HEADER]).to.match(UUID_REGEX)
   })
 
   it('should set Request-Id and Root-Request-Id headers with the same value', () => {
