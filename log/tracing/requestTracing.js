d1b567c3a2326f22dde69ad39ccfbc85c9ae8ee1
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-08 15:31:49 +0100

    Set new request tracing headers if existing values are blank or invalid

--- log/tracing/requestTracing.js
+++ log/tracing/requestTracing.js
@@ -2,12 +2,31 @@ const uuid = require('uuid')
 
 const { REQUEST_ID_HEADER, ROOT_REQUEST_ID_HEADER, ORIGIN_REQUEST_ID_HEADER } = require('./headers')
 
+const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+
+const setNewRequestTracingHeaders = (req) => {
+  const id = uuid()
+  req.headers[REQUEST_ID_HEADER] = id
+  req.headers[ROOT_REQUEST_ID_HEADER] = id
+  req.headers[ORIGIN_REQUEST_ID_HEADER] = undefined
+}
+
+const tracingHeadersNotPresentOrInvalid = (req) => {
+  return notUUID(req.headers[REQUEST_ID_HEADER])
+    || notUUID(req.headers[ROOT_REQUEST_ID_HEADER])
+    || notUUID(req.headers[ORIGIN_REQUEST_ID_HEADER])
+}
+
+const notUUID = (uuidString) => {
+  return !UUID_REGEX.test(uuidString)
+}
+
+
 class RequestTracing {
   static middleware (req, res, next) {
-    const id = uuid()
-    req.headers[REQUEST_ID_HEADER] = id
-    req.headers[ROOT_REQUEST_ID_HEADER] = id
-    req.headers[ORIGIN_REQUEST_ID_HEADER] = undefined
+    if (tracingHeadersNotPresentOrInvalid(req)) {
+      setNewRequestTracingHeaders(req)
+    }
     next()
   }
 }
--- test/unit/tracing/requestTracing.js
+++ test/unit/tracing/requestTracing.js
@@ -1,16 +1,19 @@
 'use strict'
 /* global describe, context, it, beforeEach */
 
+const uuid = require('uuid')
+
 const { expect, sinon } = require('../../chai-sinon')
 
 const RequestTracing = require('../../../log/tracing/requestTracing')
 const { REQUEST_ID_HEADER, ROOT_REQUEST_ID_HEADER, ORIGIN_REQUEST_ID_HEADER } = require('../../../log/tracing/headers')
 
-const UUID_REGEX = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 
 describe('RequestTracing', () => {
   describe('middleware', () => {
     const requestTracingMiddleware = RequestTracing.middleware
+    const GIBBERISH = 'gibberish'
     let req, res, next
 
     beforeEach(() => {
@@ -21,12 +24,7 @@ describe('RequestTracing', () => {
       next = sinon.stub()
     })
 
-    it('should call the next function', () => {
-      requestTracingMiddleware(req, res, next)
-      expect(next).calledOnce
-    })
-
-    context(`when ${REQUEST_ID_HEADER} is not initially present`, () => {
+    function itShouldSetNewRequestIdValues() {
       it(`should set ${REQUEST_ID_HEADER} header with UUID`, () => {
         requestTracingMiddleware(req, req, next)
         expect(req.headers[REQUEST_ID_HEADER]).to.match(UUID_REGEX)
@@ -57,6 +55,60 @@ describe('RequestTracing', () => {
         req.headers[ORIGIN_REQUEST_ID_HEADER] = 'gibberish'
         requestTracingMiddleware(req, req, next)
         expect(req.headers[ORIGIN_REQUEST_ID_HEADER]).to.be.undefined
+      })
+    }
+
+    it('should call the next function', () => {
+      requestTracingMiddleware(req, res, next)
+      expect(next).calledOnce
+    })
+
+    context(`when ${REQUEST_ID_HEADER} is not initially present`, () => {
+      itShouldSetNewRequestIdValues()
+    })
+
+    context(`when ${REQUEST_ID_HEADER} is not a valid UUID`, () => {
+      beforeEach(() => {
+        req.headers[REQUEST_ID_HEADER] = GIBBERISH
+      })
+
+      itShouldSetNewRequestIdValues()
+    })
+
+    context(`when ${REQUEST_ID_HEADER} is valid UUID but ${ROOT_REQUEST_ID_HEADER} is not`, () => {
+      beforeEach(() => {
+        req.headers[REQUEST_ID_HEADER] = uuid()
+        req.headers[ROOT_REQUEST_ID_HEADER] = GIBBERISH
+      })
+
+      itShouldSetNewRequestIdValues()
+    })
+
+    context(`when ${REQUEST_ID_HEADER} is valid UUID but ${ORIGIN_REQUEST_ID_HEADER} is not`, () => {
+      beforeEach(() => {
+        req.headers[REQUEST_ID_HEADER] = uuid()
+        req.headers[ORIGIN_REQUEST_ID_HEADER] = GIBBERISH
+      })
+
+      itShouldSetNewRequestIdValues()
+    })
+
+    context('when all header values are valid UUIDs', () => {
+      const requestId = uuid()
+      const rootRequestId = uuid()
+      const originRequestId = uuid()
+
+      beforeEach(() => {
+        req.headers[REQUEST_ID_HEADER] = requestId
+        req.headers[ROOT_REQUEST_ID_HEADER] = rootRequestId
+        req.headers[ORIGIN_REQUEST_ID_HEADER] = originRequestId
+      })
+
+      it('should keep those values', () => {
+        requestTracingMiddleware(req, req, next)
+        expect(req.headers[REQUEST_ID_HEADER]).to.equal(requestId)
+        expect(req.headers[ROOT_REQUEST_ID_HEADER]).to.equal(rootRequestId)
+        expect(req.headers[ORIGIN_REQUEST_ID_HEADER]).to.equal(originRequestId)
       })
     })
   })
