7840392f321cba1962962017894574c7acf0955d
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-08 12:46:45 +0100

    Update tracing middleware tests

--- test/unit/tracing/middleware.js
+++ test/unit/tracing/middleware.js
@@ -4,7 +4,7 @@
 const { expect, sinon } = require('../../chai-sinon')
 
 const { requestTracingMiddleware } = require('../../../log/tracing/middleware')
-const { REQUEST_ID_HEADER, ROOT_REQUEST_ID_HEADER } = require('../../../log/tracing/headers')
+const { REQUEST_ID_HEADER, ROOT_REQUEST_ID_HEADER, ORIGIN_REQUEST_ID_HEADER } = require('../../../log/tracing/headers')
 
 describe('requestTracingMiddleware', () => {
   let req, res, next
@@ -22,9 +22,18 @@ describe('requestTracingMiddleware', () => {
     expect(next).calledOnce
   })
 
-  it('should set requestId header', () => {
+  it('should set Request-Id header', () => {
     requestTracingMiddleware(req, req, next)
     expect(req.headers[REQUEST_ID_HEADER]).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
+  })
+
+  it('should set Root-Request-Id header', () => {
+    requestTracingMiddleware(req, req, next)
     expect(req.headers[ROOT_REQUEST_ID_HEADER]).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
+  })
+
+  it('should not set Origin-Request-Id header', () => {
+    requestTracingMiddleware(req, req, next)
+    expect(req.headers[ORIGIN_REQUEST_ID_HEADER]).to.be.undefined
   })
 })
