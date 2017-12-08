ee5379c399dbdee028c7415ee834475c41e4ff66
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-08 12:57:15 +0100

    Export tracing features

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 
 let Logger = require('./log/Logger');
 Logger.express = require('./log/express');
+Logger.requestTracing = {
+  headers: require('./log/tracing/headers'),
+  middleware: require('./log/tracing/middleware')
+}
 
 module.exports = Logger;
