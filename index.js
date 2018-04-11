df034dc0b599d712120cb6636e4a4dbc51fc6eb1
Damian Dunajski <damian.dunajski@hmcts.net>
2018-04-11 16:53:17 +0100

    Remove request tracing middleware
    Middleware is no longer needed in CNP world

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 
 module.exports = {
   Logger: require('./log/Logger'),
-  RequestTracing: require('./log/tracing/requestTracing'),
-  RequestTracingHeaders: require('./log/tracing/headers'),
   Express: require('./log/express')
 }
