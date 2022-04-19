b583a25c3a9d03f96e102ecefad122948f64a627
Linus Norton <linusnorton@gmail.com>
2022-04-19 11:22:05 +0100

    Upgrade winston version

--- test/unit/loggerTest.js
+++ test/unit/loggerTest.js
@@ -1,128 +1,23 @@
 'use strict'
 /* global beforeEach, before, after, describe, it */
 
-const { expect, assert, sinon } = require('../chai-sinon')
-const spyLogger = require('winston-spy')
+const { expect } = require('../chai-sinon')
 const winston = require('winston')
-
-const myLogger = require('../../log/Logger')
-
 
 describe('Logging within the Node.js application', () => {
 
-  describe('Logging an event at a given level', () => {
+  describe('Creating a logger', () => {
+    const Logger = require('../../log/Logger')
+    const instance = Logger.getLogger('test')
 
-    const testMessage = 'Hello World'
-    const testMeta = { hello: 'world' }
-    let logger
-    let i = 0
-    let spy
-
-    beforeEach(() => {
-      logger = myLogger.getLogger('test' + i++)
-      logger.remove(winston.transports.Console)
-      spy = sinon.spy()
+    it('should set the transport to console', () => {
+      expect(instance.transports[0]).to.be.instanceOf(winston.transports.Console)
     })
 
-    afterEach(() => {
-      logger.remove(spyLogger)
+    it('should set default the level to INFO', () => {
+      expect(instance.transports[0].level).to.eq('info')
     })
 
-    context('when logger default level is DEBUG', () => {
-      beforeEach(() => {
-        logger.add(spyLogger, { level: 'debug', spy: spy })
-      })
-
-      it('should not log a message for SILLY', () => {
-        logger.silly(testMessage, testMeta)
-
-        assert(spy.notCalled)
-      })
-
-      it('should log a message for VERBOSE', () => {
-        logger.verbose(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('verbose', testMessage, testMeta))
-      })
-
-      it('should log a message for INFO', () => {
-        logger.info(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('info', testMessage, testMeta))
-      })
-
-      it('should log a message for WARN', () => {
-        logger.warn(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('warn', testMessage, testMeta))
-      })
-
-      it('should log a message for ERROR', () => {
-        logger.error(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('error', testMessage, testMeta))
-      })
-    })
-
-    context('when logger default level matches level used to log the message', () => {
-      it('should log a message at level SILLY', () => {
-        logger.add(spyLogger, { level: 'silly', spy: spy })
-
-        logger.silly(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('silly', testMessage, testMeta))
-      })
-
-      it('should log a message at level DEBUG', () => {
-        logger.add(spyLogger, { level: 'debug', spy: spy })
-
-        logger.debug(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('debug', testMessage, testMeta))
-      })
-
-      it('should log a message at level VERBOSE', () => {
-        logger.add(spyLogger, { level: 'verbose', spy: spy })
-
-        logger.verbose(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('verbose', testMessage, testMeta))
-      })
-
-      it('should log a message at level INFO', () => {
-        logger.add(spyLogger, { level: 'info', spy: spy })
-
-        logger.info(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('info', testMessage, testMeta))
-      })
-
-      it('should log a message at level WARN', () => {
-        logger.add(spyLogger, { level: 'warn', spy: spy })
-
-        logger.warn(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('warn', testMessage, testMeta))
-      })
-
-      it('should log a message at level ERROR', () => {
-        logger.add(spyLogger, { level: 'error', spy: spy })
-
-        logger.error(testMessage, testMeta)
-
-        assert(spy.calledOnce)
-        assert(spy.calledWith('error', testMessage, testMeta))
-      })
-    })
   })
 
   describe('Obtaining a single instance of Logger', () => {
